b4448a4c1067d230cf8b0439f0636ac474101a0b
Chandana BM <chandana66266@gmail.com>
2026-08-01 12:59:46 +0530

    done

---
+++ class_01/script.js
@@ -0,0 +1,47 @@
+// let a = 20;
+// let b = "100";
+// console.log((b+b));
+
+// let a = 10;
+// let b = 20;
+// console.log("sum of 10 and 20 (a+b) " + (a+b));
+
+// console.log("1" / 1);
+
+// let age = prompt('please enter your age');
+// age = Number(age);
+// console.log(typeof age);
+
+// let age = Number (prompt('please enter your age'));
+// console.log(typeof age);
+
+// type casting
+
+// swap
+// method 1
+
+// let a = 10;
+// let b = 20;
+
+// let temp= a;
+// a = b;
+// b= temp;
+// console.log(b);
+// console.log(a);
+// console.log(temp);
+
+// method 2
+// let a = 10;
+// let b = 20;
+// // a = a + b;
+// // b = a - b;
+// // a = a - b;
+// // console.log(a);
+// // console.log(b);
+
+// [a,b]=[b,a]
+// console.log(a,b);
+ let i = 11;
+ i = i++ + ++i;
+ console.log(i);
+ 
