b8c18f2feb9473b140fdfb9887c9a53b7e16f708
chandana <chandana66266@gmail.com>
2026-08-03 22:10:37 +0530

    commit is done

---
+++ class_02/script.js
@@ -0,0 +1,14 @@
+//  age = Number( prompt('enter  your age '));
+
+// if (age >=18){
+//     console.log("you are eligible");
+
+// }
+// else{
+//     console.log('not eligible');
+    
+// }
+for (let i =1 ;i <=5; i++){
+    console.log("hey this is loop");
+}
+    
